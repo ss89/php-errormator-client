2303d43f630160d8f0d00d415a2657d6c9aae2dc
webdev <webdev@bcs-020b.bcsnetcom.loc>
2013-06-13 17:53:36 +0200

    Refactoring of the Class Applied Coding Style UUID4 Support added automatically generate UUID4 if there is none for a report/log added ability to set API Version (defaults to 0.3) deleted checkApiKey check from constructor, as it calls too often - just use the public function of you want to check validity of your key. checkApiKey now adds a Log Entry.

--- ErrormatorClient.php
+++ ErrormatorClient.php
@@ -11,49 +11,61 @@ class ErrormatorClient
 
     public $curl;
     public $scheme;
-    public $api_key;
-    public $debug=false;
+    public $apiKey;
+    public $debug = false;
+    private $apiVersion = "0.3";
 
     public function __construct($data)
     {
-        $check = $this->__checkPrerequisites();
+        $check = $this->checkPrerequisites();
         if (!$check)
         {
-            throw new CurlNotFound;
+            throw new Exception("Curl not Found in your Configuration or not loaded. Please load the curl.(so/dll) and restart you Web server/PHP Fast CGI");
         }
+        $this->setDefaultSettings($data);
+    }
+
+    private function setDefaultSettings($data)
+    {
         $this->scheme = $data['scheme'];
-        $this->api_key = $data['api_key'];
+        $this->apiKey = $data['api_key'];
         $this->curl = curl_init();
-        if(isset($data['debug']) && $data['debug']===true)
+        if (isset($data['debug']) && $data['debug'] === true)
         {
-            $this->debug=true;
+            $this->debug = true;
         }
-        curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'X-errormator-api-key: ' . $this->api_key));
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'X-errormator-api-key: ' . $this->apiKey));
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->curl, CURLOPT_POST, true);
-        $api_key_valid=false;
-        $api_key_valid=  $this->__checkApiKey();
-        if(!$api_key_valid)
-        {
-            if($this->debug===true)
-            {
-                echo "<br>API KEY NOT VALID<br>";
-            }
-            return false;
-        }
     }
-    
-    public function __checkApiKey()
+
+    public function genUuid()
     {
-//        403 Forbidden
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
+    }
+
+    public function checkApiKey()
+    {
+//      403 Forbidden = API Key not valid
+        array(
+            array(
+                "client" => "php-ss89",
+                "log_level" => "INFO",
+                "message" => "Testing API Key",
+                "server" => $_SERVER["HTTP_HOST"],
+                "date" => date("d.m.Y H:i:s"),
+                "namespace" => "ErrormatorClient",
+                "request_id" => $this->genUuid(),
+            )
+        );
         curl_setopt($this->curl, CURLOPT_HEADER, true);
-        $response=  $this->send_curl_request($this->scheme . "://api.errormator.com/api/slow_reports?protocol_version=0.3", array());
+        $response = $this->sendCurlRequest($this->scheme . "://api.errormator.com/api/logs?protocol_version=" . $this->apiVersion, $data);
         curl_setopt($this->curl, CURLOPT_HEADER, false);
-        if($this->debug===true)
+        if ($this->debug === true)
         {
-            echo "API KEY VALID CHECK RESPONDED: ".$response."<br>";
+            echo "API KEY VALID CHECK RESPONDED: " . $response . "<br>";
         }
-        if(strstr($response,"403 Forbidden"))
+        if (strstr($response, "403 Forbidden"))
         {
             return false;
         }
@@ -63,7 +75,7 @@ class ErrormatorClient
         }
     }
 
-    public function __checkPrerequisites()
+    public function checkPrerequisites()
     {
         return function_exists("curl_init");
     }
@@ -71,9 +83,29 @@ class ErrormatorClient
     public function slow($data)
     {
         $return = false;
-        if ($this->scheme)
+        if (is_array($data))
         {
-            $return = $this->send_curl_request($this->scheme . "://api.errormator.com/api/slow_reports?protocol_version=0.3", $data);
+            foreach ($data as $key => $value)
+            {
+                if (is_array($value['report_details']))
+                {
+                    foreach ($value['report_details'] as $ukey => $uvalue)
+                    {
+                        if (!isset($uvalue['request_id']) || empty($uvalue['request_id']))
+                        {
+                            $data[$key]['report_details'][$ukey]['request_id'] = $this->genUuid();
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            if ($this->scheme)
+            {
+                $return = $this->sendCurlRequest($this->scheme . "://api.errormator.com/api/slow_reports?protocol_version=" . $this->apiVersion, $data);
+            }
         }
         return $return;
     }
@@ -81,9 +113,29 @@ class ErrormatorClient
     public function error($data)
     {
         $return = false;
-        if ($this->scheme)
+        if (is_array($data))
         {
-            $return = $this->send_curl_request($this->scheme . "://api.errormator.com/api/reports?protocol_version=0.3", $data);
+            foreach ($data as $key => $value)
+            {
+                if (is_array($value['report_details']))
+                {
+                    foreach ($value['report_details'] as $ukey => $uvalue)
+                    {
+                        if (!isset($uvalue['request_id']) || empty($uvalue['request_id']))
+                        {
+                            $data[$key]['report_details'][$ukey]['request_id'] = $this->genUuid();
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            if ($this->scheme)
+            {
+                $return = $this->sendCurlRequest($this->scheme . "://api.errormator.com/api/reports?protocol_version=" . $this->apiVersion, $data);
+            }
         }
         return $return;
     }
@@ -91,23 +143,44 @@ class ErrormatorClient
     public function log($data)
     {
         $return = false;
-        if ($this->scheme)
+        if (is_array($data))
         {
-            $return = $this->send_curl_request($this->scheme . "://api.errormator.com/api/logs?protocol_version=0.3", $data);
+            foreach ($data as $key => $value)
+            {
+                if (!isset($value['request_id']) || empty($value['request_id']))
+                {
+                    $data[$key]['request_id'] = $this->genUuid();
+                }
+            }
+            if ($this->scheme)
+            {
+                $return = $this->sendCurlRequest($this->scheme . "://api.errormator.com/api/logs?protocol_version=" . $this->apiVersion, $data);
+            }
         }
         return $return;
     }
 
-    public function send_curl_request($url, $data)
+    public function sendCurlRequest($url, $data)
     {
         curl_setopt($this->curl, CURLOPT_URL, $url);
         $json_data = json_encode($data);
-        if($this->debug===true)
+        if ($this->debug === true)
         {
             echo "<br>" . $json_data . "<br>";
         }
         curl_setopt($this->curl, CURLOPT_POSTFIELDS, $json_data);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
         return curl_exec($this->curl);
+    }
+
+    public function getApiVersion()
+    {
+        return $this->apiVersion;
+    }
+
+    public function setApiVersion($version)
+    {
+        return $this->apiVersion = $version;
     }
 
 }
